833716c760eebd7a80a7ebc7bd4355565a437e05
DESKTOP-PKH4CN4\37533 <Kostya-voronkov1992@rambler.ru>
2023-01-10 01:22:43 +0300

    add local project

---
+++ JS/burger.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#burger").addEventListener("click", function () {
+    document.querySelector("#nav__list").classList.toggle("is-active");
+    document.querySelectorAll(".header__contact").forEach(function (element) {
+      element.classList.toggle("is-active");
+    });
+    document.querySelector("#burger").classList.toggle("burger-close");
+  });
+});
